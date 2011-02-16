4eb6451df5e8705eeff0f4690f3ad4f48e1eb432
Thomas Wolfe <tomwolfe@gmail.com>
2011-02-15 22:33:47 -0600

    created user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :current_courier
+  helper_method :current_courier, :current_user
   
   private
   
@@ -13,5 +13,15 @@ class ApplicationController < ActionController::Base
   def current_courier
     return @current_courier if defined?(@current_courier)
     @current_courier = current_courier_session && current_courier_session.record
+  end
+  
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+  
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110210002440) do
+ActiveRecord::Schema.define(:version => 20110216034532) do
 
   create_table "couriers", :force => true do |t|
     t.string   "email"
@@ -57,6 +57,21 @@ ActiveRecord::Schema.define(:version => 20110210002440) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "max_distance"
+  end
+
+  create_table "user_sessions", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
