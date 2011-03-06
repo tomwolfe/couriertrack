47fec229e82413d1c9f34f5958e3dbce9d29c164
Thomas Wolfe <tomwolfe@gmail.com>
2011-03-05 22:52:39 -0600

    using distance for the model name no longer seemed right... for now search_courier I guess will be fine

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -3,7 +3,7 @@ class Courier < ActiveRecord::Base
 	acts_as_mappable :default_units => :kms
 	
 	has_many :deliveries, :dependent => :destroy
-	has_many :distances, :dependent => :destroy
+	has_many :search_couriers, :dependent => :destroy
 	
 	def add_delivery_mass_and_volume(delivery)
 		update_attributes(:current_mass => current_mass + delivery.mass, :current_volume => current_volume + delivery.volume)
