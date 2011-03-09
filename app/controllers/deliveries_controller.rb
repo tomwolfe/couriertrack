a16f0dd1916f66a0baec0f09e9da0e2b1c91851c
Thomas Wolfe <tomwolfe@gmail.com>
2011-03-09 05:37:38 -0600

    using transactions now for update/save deliveries_controller to check if the delivery and courier are valid before saving both, or not saving both. added some validation to the courier model. modified the deliveries _form.html.erb to show errors for both models.

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -46,16 +46,20 @@ class DeliveriesController < ApplicationController
   def create
   	@courier = Courier.find(params[:courier_id])
     @delivery = @courier.deliveries.build(params[:delivery])
-    @delivery.set_coordinates
+    @delivery.set_dropoff_coordinates
     @courier.add_delivery_mass_and_volume(@delivery)
 
     respond_to do |format|
-      if @delivery.save
-        format.html { redirect_to(courier_delivery_path(@courier, @delivery), :notice => 'Delivery was successfully created.') }
-        format.xml  { render :xml => @delivery, :status => :created, :location => @delivery }
+      if @delivery.valid? & @courier.valid?
+      	Delivery.transaction do
+      		@courier.save!
+      		@delivery.save!
+      		format.html { redirect_to(courier_delivery_path(@courier, @delivery), :notice => 'Delivery was successfully created.') }
+        	format.xml  { render :xml => @delivery, :status => :created, :location => @delivery }
+      	end
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @delivery.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @delivery.errors + @courier.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -66,16 +70,21 @@ class DeliveriesController < ApplicationController
   	@courier = Courier.find(params[:courier_id])
     @delivery = @courier.deliveries.find(params[:id])
     @courier.remove_delivery_mass_and_volume(@delivery)
+    @delivery.attributes = params[:delivery] # rather than update_attributes so not saved until transaction
+    @courier.add_delivery_mass_and_volume(@delivery)
+    @delivery.set_coordinates
 
     respond_to do |format|
-      if @delivery.update_attributes(params[:delivery])
-      	@courier.add_delivery_mass_and_volume(@delivery)
-      	@delivery.set_coordinates
-        format.html { redirect_to(courier_delivery_path(@courier, @delivery), :notice => 'Delivery was successfully updated.') }
-        format.xml  { head :ok }
+      if @delivery.valid? & @courier.valid?
+      	Delivery.transaction do
+      		@courier.save!
+      		@delivery.save!
+      		format.html { redirect_to(courier_delivery_path(@courier, @delivery), :notice => 'Delivery was successfully updated.') }
+        	format.xml  { head :ok }
+      	end
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @delivery.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @delivery.errors + @courier.errors, :status => :unprocessable_entity }
       end
     end
   end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,13 +1,10 @@
 class Delivery < ActiveRecord::Base
 	acts_as_mappable :default_units => :kms
 	belongs_to :courier
-	validates :mass, :numericality => {:less_than_or_equal_to => @courier.avail_mass}
-	validates :volume, :numericality => {:less_than_or_equal_to => @courier.avail_volume}
 	
-	TRANSPORT_MODES = [ "Bicycle", "Car", "Truck", "Walking" ]
-	
-	def set_coordinates
+	def set_dropoff_coordinates
 		loc = GeoKit::Geocoders::MultiGeocoder.geocode(dropoff_address)
-	    update_attributes(:lat => loc.lat, :lng => loc.lng)
+		lat = loc.lat
+	    lng = loc.lng
 	end
 end
